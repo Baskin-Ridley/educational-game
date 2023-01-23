64de6d3e83acb67f43cfb37780a34bf9521ba0ec
Baskin-Ridley <gbaskinridley@gmail.com>
2023-01-23 14:27:20 +0000

    test runs

--- client/questionPage/question.test.js
+++ client/questionPage/question.test.js
@@ -2,6 +2,12 @@
  * @jest-environment jsdom
  */
 
+ const fetchMock = require("jest-fetch-mock");
+ const fs = require('fs');
+ const path = require('path');
+ 
+ fetchMock.enableMocks();
+
 const {addQuestion, checkAnswer, displayScore} = require('./question')
 
 describe("addQuestion", () => {
@@ -11,3 +17,26 @@ describe("addQuestion", () => {
     })
 
 })
+/*
+test('addQuestion', () => {
+    expect(addQuestion).toBeDefined();
+});
+
+describe("checkAnswer", () => {
+    
+        it("exists",() => {
+            expect(checkAnswer).toBeDefined();
+        })
+    
+    }
+)
+
+describe("displayScore", () => {
+
+    it("exists",() => {
+        expect(displayScore).toBeDefined();
+    })
+
+}
+)
+*/
