c5ab8e4eebdf1cd58309728878aaa4401a515351
“sadik312” <sadikislam46@gmail.com>
2023-01-23 14:20:02 +0000

    create js func for leaderboard, currently using test-page.html as dummy pages

--- client/homePage/index.js
+++ client/homePage/index.js
@@ -7,3 +7,8 @@ document.getElementById(userRecord).addEventListener("click", userRecord);
 function userRecord() {
     window.location.href="test-page.html";
 }
+
+document.getElementById(leaderBoard).addEventListener("click", leaderBoard);
+function leaderBoard() {
+    window.location.href="test-page.html";
+}
