3ecd312a4cba072d4e659c4132059a0b2a8fce7e
Baskin-Ridley <gbaskinridley@gmail.com>
2023-01-23 10:03:05 +0000

    temp question file

---
+++ client/questionPage/question.js
@@ -0,0 +1,8 @@
+const goats = [
+    {
+        "id": 1,
+        "question": "Who's that pokemon? (hint: it's a Jigglypuff)",
+        "answers": ["1", "2", "3", "4"],
+        "correct": "3"
+        }
+    ]
---
+++ client/questionPage/temp.js
@@ -0,0 +1,10 @@
+const temp = [
+    {
+        "id": 1,
+        "question": "Who's that pokemon? (hint: it's a Jigglypuff)",
+        "answers": ["1", "2", "3", "4"],
+        "correct": "3"
+        }
+    ]
+
+    module.exports = {temp}
