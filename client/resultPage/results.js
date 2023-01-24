1e58c540bfc9512f5735d68b3b9768fe582aa265
Cameron <amoatengcameron@hotmail.com>
2023-01-24 10:17:47 +0000

    pull from main and merge code

--- client/resultPage/results.js
+++ client/resultPage/results.js
@@ -1,30 +1,26 @@
 
 let score = 0
 
-function dispScore (){
 
-
-const score = require("./questionPage/question.js");
-console.log(score);
 
 document.querySelector("#retry").addEventListener("click", qPage);
 function qPage() {
-    window.location.href="questionPage.html";
+    window.location.href="../questionPage/questionPage.html";
 }
 document.querySelector("#home").addEventListener("click", hPage);
 function hPage() {
-    window.location.href="index.html";
+    window.location.href="../homePage/index.html";
 }
 document.querySelector("#leader").addEventListener("click", lPage);
 function lPage() {
-    window.location.href="questionPage.html";
+    window.location.href="../questionPage/questionPage.html";
 }
 function displayScore () {
     const card = document.createElement("div")
     card.textContent = score;
     document.getElementById('score').appendChild(card)
 }
-}
+
 
 displayScore()
 module.exports = {
